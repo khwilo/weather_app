2cb96cf50e31a859fabdc3e364ab648fa76960a2
khwilo <khwilowatai@gmail.com>
2019-03-28 14:38:51 +0300

    Move the App.css import to the App component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-import './App.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
