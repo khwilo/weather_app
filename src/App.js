7d05522fa38c1ce1e61e90ded62e8f3c1e01fd39
khwilo <khwilowatai@gmail.com>
2019-03-28 12:35:41 +0300

    Add the weather attributes as state in App

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ import Weather from "./components/Weather";
 const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 class App extends Component {
+  state = {
+    temperature: undefined,
+    city       : undefined,
+    country    : undefined,
+    humidity   : undefined,
+    description: undefined,
+    error      : undefined,
+  }
+
   getWeather = async (e) => {
     e.preventDefault();
 
@@ -19,13 +28,29 @@ class App extends Component {
     const data = await api_call.json();
 
     console.log(data);
+
+    this.setState({
+      temperature: data.main.temp,
+      city       : data.name,
+      country    : data.sys.country,
+      humidity   : data.main.humidity,
+      description: data.weather[0].description,
+      error      : "",
+    });
   }
+
   render() {
     return (
       <div>
         <Titles />
         <Form getWeather={this.getWeather} />
-        <Weather />
+        <Weather
+          temperature = {this.state.temperature}
+          city        = {this.state.city}
+          country     = {this.state.country}
+          humidity    = {this.state.humidity}
+          description = {this.state.description}
+          error       = {this.state.error} />
       </div>
     );
   }
