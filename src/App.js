f8a7acd6de555a1eec35b1d74dfad9f1cf4168d2
khwilo <khwilowatai@gmail.com>
2019-03-28 11:56:58 +0300

    Fetch the city and country values from the form

--- src/App.js
+++ src/App.js
@@ -10,8 +10,11 @@ class App extends Component {
   getWeather = async (e) => {
     e.preventDefault();
 
+    const city    = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
     const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=Kiambu,ke&appid=${WEATHER_API_KEY}&units=metric`);
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${WEATHER_API_KEY}&units=metric`);
 
     const data = await api_call.json();
 
