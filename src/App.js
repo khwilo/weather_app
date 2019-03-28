63191dae968975a84ac7b21a93d3c516636a6d3a
khwilo <khwilowatai@gmail.com>
2019-03-28 11:53:22 +0300

    Create a method to fetch the weather properties

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import Form from "./components/Form";
 import Titles from "./components/Titles";
 import Weather from "./components/Weather";
 
+const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
 class App extends Component {
+  getWeather = async (e) => {
+    e.preventDefault();
+
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=Kiambu,ke&appid=${WEATHER_API_KEY}&units=metric`);
+
+    const data = await api_call.json();
+
+    console.log(data);
+  }
   render() {
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     );
