7d4f2ba531be32fbf2241213b617f349e6aff2b7
khwilo <khwilowatai@gmail.com>
2019-03-28 12:36:50 +0300

    Display the weather attributes

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,7 +3,12 @@ import React, { Component } from "react";
 class Weather extends Component {
     render() {
         return (
-            <div>Weather Component</div>
+            <div>
+                <p>Location   : { this.props.city }, { this.props.country }</p>
+                <p>Temperature: { this.props.temperature }</p>
+                <p>Humidity   : { this.props.humidity }</p>
+                <p>Conditions : { this.props.description }</p>
+            </div>
         );
     }
 }
