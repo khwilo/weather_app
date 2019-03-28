ee5697e7f4c693ddf94aceb60f57cc8b940fe4d4
khwilo <khwilowatai@gmail.com>
2019-03-28 13:05:50 +0300

    Use an implicit return statement

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-const Titles = () => {
-    return (
-        <div>
-            <h1>Weather Finder</h1>
-            <p>Find out temperature conditions and more...</p>
-        </div>
-    );
-};
+const Titles = () => (
+    <div>
+        <h1>Weather Finder</h1>
+        <p>Find out temperature conditions and more...</p>
+    </div>
+);
 
 export default Titles;
